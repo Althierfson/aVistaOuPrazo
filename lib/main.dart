55fa7b17e7291c66425a686ba6a9e5cde675a083
althierfson <althierfson.lima.109@ufrn.edu.br>
2023-02-08 20:07:36 -0300

    fist commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:avistaouaprazo/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: theme,
+    home: const Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Column(children: [
+          TextField()
+        ],),
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme = ThemeData(
+  inputDecorationTheme: const InputDecorationTheme(border: OutlineInputBorder())
+);
